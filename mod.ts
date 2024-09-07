c4e583007446da2ed0282fc22bb3c1d18f9ae80e
stallone-dev <stallone.developer@gmail.com>
2024-09-07 20:58:29 -0300

    docs(project): Change license to `MPL-2.0` standard
    Explanation: When evaluating the applicability of this project, I assessed that it would be healthier for the project to change the license to this better structured standard

--- mod.ts
+++ mod.ts
@@ -1,4 +1,8 @@
-import type * as MtrWSType from "./index.ts";
+/*
+    This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+*/
+
+import type * as MtrWSType from "./api-types.ts";
 import { API_ROUTE } from "./api-routes.ts";
 import { API_BASE_URL } from "./api-url.ts";
 
@@ -6,20 +10,20 @@ export {
     /**
      * Lista de URLs de produção da API WebService do SINIR/SIGOR para controle de MTRs
      * @See Referência para construção destas interfaces: {@link https://cetesb.sp.gov.br/sigor-mtr/web-service/}
-     * @version SIGOR - 1.15.0 - ago/24
+     * @version SIGOR - 1.15.0 - SIGOR ago/24
      */
     API_BASE_URL as MtrWSBaseURL,
     /**
      * Lista de rotas REST disponveis na API WebService do SINIR/SIGOR para controle de MTRs
      * @See Referência para construção destas interfaces: {@link https://cetesb.sp.gov.br/sigor-mtr/web-service/}
-     * @version SIGOR - 1.15.0 - ago/24
+     * @version SIGOR - 1.15.0 - SIGOR ago/24
      */
     API_ROUTE as MtrWSRoute,
     /**
      * Interfaces para estruturação e análise da API WebService para controle de MTRs do SINIR/SIGOR
      *
      * @See Referência para construção destas interfaces: {@link https://cetesb.sp.gov.br/sigor-mtr/web-service/}
-     * @version SIGOR - 1.15.0 - ago/24
+     * @version SIGOR - 1.15.0 - SIGOR ago/24
      */
     type MtrWSType,
 };
